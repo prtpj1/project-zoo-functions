1c3132866ef21108e1660d26c279b47fd6954153
Paulo Porto <prtpj1@gmail.com>
2022-01-26 20:10:53 -0300

    Req 1 Done

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids === '' || ids === undefined){
+    return [];
+  }
+  
+  return species.filter((animal) => ids.includes(animal.id));
 }
 
 module.exports = getSpeciesByIds;
+
