8dd045b7799eaf39b08a5eb3051ab489d0dfda4a
Paulo Porto <prtpj1@gmail.com>
2022-01-26 20:20:36 -0300

    ESLint didnt load properly

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,12 +2,11 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  if (ids === '' || ids === undefined){
+  if (ids === '' || ids === undefined) {
     return [];
   }
-  
+
   return species.filter((animal) => ids.includes(animal.id));
 }
 
 module.exports = getSpeciesByIds;
-
