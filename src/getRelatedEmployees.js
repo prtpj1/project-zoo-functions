fd7c39835b5926d49405100900abd91f298a7061
Paulo Porto <prtpj1@gmail.com>
2022-01-29 16:12:00 -0300

    Req 4 Done

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,19 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((manager) => manager.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return employees
+    .filter((filterManager) => filterManager.managers
+      .find((findManager) => findManager === managerId))
+    .map((mapManager) =>
+      `${mapManager.firstName} ${mapManager.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
