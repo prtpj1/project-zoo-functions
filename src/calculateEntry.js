1b5df25a0028f5ba15525e68cb8ce4cee6658b17
Paulo Porto <prtpj1@gmail.com>
2022-01-30 21:11:07 -0300

    Req 6 done

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,32 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  return entrants.reduce((acc, cur) => {
+    if (cur.age < 18) {
+      acc.child += 1;
+    }
+
+    if (cur.age >= 18 && cur.age < 50) {
+      acc.adult += 1;
+    }
+
+    if (cur.age >= 50) {
+      acc.senior += 1;
+    }
+    return acc;
+  }, { adult: 0, child: 0, senior: 0 });
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+
+  const { adult, child, senior } = countEntrants(entrants);
+  const { adult: adultTicket, child: childTicket, senior: seniorTicket } = prices;
+
+  return ((adultTicket * adult) + (childTicket * child) + (seniorTicket * senior));
 }
 
 module.exports = { calculateEntry, countEntrants };
