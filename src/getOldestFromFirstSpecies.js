cd1cebc31c28ce5725fd93bfa3a2be309586029b
Paulo Porto <prtpj1@gmail.com>
2022-01-31 22:30:51 -0300

    Req 9 Done

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const idFunc = employees.find((elemId) => elemId.id === id)
+    .responsibleFor.find((elemAnimal) => elemAnimal);
+
+  const getSpecie = species.find((eleSpecie) => eleSpecie.id === idFunc)
+    .residents;
+
+  const sortResidents = getSpecie.sort((a, b) => a.age - b.age);
+  const getOld = sortResidents[sortResidents.length - 1];
+  const oldest = Object.keys(getOld).map((key) => (getOld[key]));
+  return oldest;
 }
 
+getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1');
 module.exports = getOldestFromFirstSpecies;
